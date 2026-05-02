2890094d203720b2c819fb01b7a39df48131db63
Adam Poit <adam.poit@logos.com>
2026-05-02 10:28:56 -0700

    Add ci workflow.

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    deploy-rs.url = "github:serokell/deploy-rs";
   };
 
   outputs = inputs @ {
     self,
     nixpkgs,
     flake-utils,
+    deploy-rs,
     ...
   }: let
     lib = nixpkgs.lib;
@@ -21,6 +23,7 @@
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.alejandra
+            deploy-rs.packages.${system}.default
           ];
         };
 
@@ -39,5 +42,14 @@
 
       packages.x86_64-linux.digitalocean-image =
         self.nixosConfigurations.digitalocean-base.config.system.build.images.digital-ocean;
+
+      deploy.nodes.test-droplet = {
+        hostname = "test-droplet";
+        sshUser = "root";
+        profiles.system = {
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.digitalocean-base;
+        };
+      };
     };
 }
